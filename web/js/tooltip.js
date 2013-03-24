477748bc4f156d09602070b62b0d3b045df1cab9
Jonas Wagner <wagnercode@gmail.com>
2013-03-24 15:45:03 +0100

    Only update visible tooltip positions

--- web/js/tooltip.js
+++ web/js/tooltip.js
@@ -1,15 +1,17 @@
 // Inspired by https://github.com/tpreusse/open-budget/blob/master/scripts/tooltip.js
 finance.tooltip = function(target) {
     var container = $('#chart'),
-        tip = $('<div class="tooltip"></div>').hide().appendTo(container);
+        tip = $('<div style="display:none" class="tooltip"></div>').appendTo(container);
 
     container.on('mousemove', function(e) {
         // Use pure javascript to improve performance
-        var width = e.pageX + tip[0].offsetWidth > window.innerWidth ?
-            -10 - tip[0].offsetWidth : 20;
         e.preventDefault();
-        tip[0].style.top =  e.pageY - 30 + 'px';
-        tip[0].style.left = e.pageX + width + 'px';
+        if(tip[0].style.display != 'none') {
+            var width = e.pageX + tip[0].offsetWidth > window.innerWidth ?
+                -10 - tip[0].offsetWidth : 20;
+            tip[0].style.top =  e.pageY - 30 + 'px';
+            tip[0].style.left = e.pageX + width + 'px';
+        }
     });
 
     target.hover(function() {
